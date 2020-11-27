92c296a967edfb9656c4cbb452c80f17442022a6
Hostim <63262178+HostimFR@users.noreply.github.com>
2020-11-27 18:30:13 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
-mkdir ~/.npm-packages ~/.npm-packages/lib
+[ ! -d "/home/container/.npm-packages" ] && mkdir ~/.npm-packages ~/.npm-packages/lib
 npm config set prefix ~/.npm-packages  
 PATH=~/..npm-packages/bin/:$PATH  
